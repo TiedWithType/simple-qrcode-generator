b8d183732515eeb46463582cfdd5ec92b04643ff
TiedWithType <kamilochman79@gmail.com>
2024-09-29 11:08:57 +0200

    deperecated warning added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { AppRootComponent } from "./components/app-root.component";
 
-AppRootComponent.init();
+alert("This web app will be rewritten to new framework soon")
+
+const agree = confirm("Do you agree to use this deprecated version?");
+
+if (agree) AppRootComponent.init();
+else {
+ alert("You must confirm user agreement");
+ window.location.href="/";
+}

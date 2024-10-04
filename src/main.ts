fcd06563129681547bd7a611da7007987f1d277c
TiedWithType <kamilochman79@gmail.com>
2024-10-04 22:40:36 +0200

    info changed

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import { AppRootComponent } from "./components/app-root.component";
 
-alert("This web app will be rewritten to new framework soon")
-
-const agree = confirm("Do you agree to use this deprecated version?");
-
-if (agree) AppRootComponent.init();
-else {
- alert("You must confirm user agreement");
- window.location.href="/";
-}
+alert("This web app version is not working anymore due to deprecated framework :( Please wait for newer version")
